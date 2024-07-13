c1bb4947f7f5f2ef56acf1df52495749d1f2a046
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-07-13 12:54:21 +0530

    commit

--- middleware.tsx
+++ middleware.tsx
@@ -1,22 +1,24 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-export async function middleware(request) {
-    const path = request.nextUrl.pathname;
+
+export async function middleware(request: any) {
+    const path = new URL(request.url).pathname;
     const token = await getToken({
         req: request,
         secret: process.env.AUTH_SECRET,
     });
-    const privatePaths = ["/dashboard", "/", "/clients", "/expenses", "/invoice", "/items", "/reports", "/settings"]; // Adjusted the path names
+    const privatePaths = ["/dashboard", "/", "/clients", "/expenses", "/invoice", "/items", "/reports", "/settings"];
 
     if (!token && privatePaths.includes(path)) {
-        return NextResponse.redirect(new URL("/auth/sign-in", request.nextUrl));
+        return NextResponse.redirect(new URL("/auth/sign-in", request.url).toString());
     }
 }
 
+
 export const config = {
     // Matcher for all paths
-    matcher: ({ req }) => {
+    matcher: ({ req }: { req: Request }) => {
         const privatePaths = ["/dashboard", "/", "/clients", "/expenses", "/invoice", "/items", "/reports", "/settings"];
         const isPrivatePath = privatePaths.some(path => req.url.startsWith(path));
         return !req.url.startsWith("/api") && isPrivatePath;
