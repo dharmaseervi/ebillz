8f13f4d0203adb85c91de085cbf7774b557b013e
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-07-13 15:54:10 +0530

    commit

--- middleware.tsx
+++ middleware.tsx
@@ -1,20 +1,25 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-
 export async function middleware(request: any) {
-    const path = new URL(request.url).pathname;
+    const path = request.nextUrl.pathname;
+    const secret = process.env.AUTH_SECRET;
+    const salt = process.env.AUTH_SALT || "default_salt"
+    if (!secret) {
+        throw new Error("AUTH_SECRET is not defined in environment variables");
+    }
     const token = await getToken({
         req: request,
-        secret: process.env.AUTH_SECRET,
+        secret,
+        salt,
     });
-    const privatePaths = ["/dashboard", "/", "/clients", "/expenses", "/invoice", "/items", "/reports", "/settings"];
+
+    const privatePaths = ["/dashboard", "/", "/clients", "/expenses", "/invoice", "/items", "/reports", "/settings"]; // Adjusted the path names
 
     if (!token && privatePaths.includes(path)) {
-        return NextResponse.redirect(new URL("/auth/sign-in", request.url).toString());
+        return NextResponse.redirect(new URL("/auth/sign-in", request.nextUrl));
     }
 }
-
 
 export const config = {
     // Matcher for all paths
