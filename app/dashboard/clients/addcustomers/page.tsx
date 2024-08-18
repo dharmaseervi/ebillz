e526b7cbc11e6ee214e09cdbf7d22df417d9df2d
dharmaseervi <dharmaseervi@dharmaseervis-MacBook-Pro.local>
2024-08-18 16:17:36 +0530

    bug fix

--- app/dashboard/clients/addcustomers/page.tsx
+++ app/dashboard/clients/addcustomers/page.tsx
@@ -4,9 +4,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      <CustomerForm cancel={function (): void {
-        throw new Error('Function not implemented.')
-      } } />
+      <CustomerForm />
     </div>
   )
 }
